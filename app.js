5943d9e70324f240182e8bd3e0830cc7d5937073
Prince <--replace-all>
2020-04-13 20:39:59 +0530

    added prettier

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-const { app, BrowserWindow } = require ('electron');
+const { app, BrowserWindow } = require('electron');
 
+const createWindow = () => {
+	// Create the browser window.
+	let win = new BrowserWindow({
+		width: 320,
+		height: 547,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
 
-const createWindow =  () => {
-  // Create the browser window.
-  let win = new BrowserWindow({
-    width: 320,
-    height: 547,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  // and load the index.html of the app.
-  win.loadFile('index.html')
-  win.setMaximizable(false);
-}
+	// and load the index.html of the app.
+	win.loadFile('index.html');
+	win.setMaximizable(false);
+};
 
 app.whenReady().then(createWindow);
 
-app.on("window-all-closed", () => {
-    app.quit();
-  });
+app.on('window-all-closed', () => {
+	app.quit();
+});
--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const clickEvent = 'click';
 
-const operatorList = [
-    'multiply', 'add', 'subtract', 'divide'
-]
+const operatorList = ['multiply', 'add', 'subtract', 'divide'];
 let restart;
 let canEnterNumber;
 let canEnterOperation;
@@ -10,165 +8,164 @@ let operand1;
 let operator;
 
 const initializeParams = () => {
-    restart = false;
-    canEnterNumber = true;
-    canEnterOperation = false;
-    operand1 = null;
-    operator = null;
-}
-
-const getResult = (operand1, operand2, operator) => {
-    switch (operator) {
-        case 'multiply': 
-            return operand1*operand2;
-        case 'add':
-            return operand1+operand2;
-        case 'subtract':
-            return operand1-operand2;
-        case 'divide':
-            return Math.floor(operand1/operand2);
-    }
-}
-
-const getSymbol = (operator) => {
-    switch (operator) {
-        case 'multiply': 
-            return 	String.fromCharCode(215);
-        case 'add':
-            return '+';
-        case 'subtract':
-            return '-';
-        case 'divide':
-            return String.fromCharCode(247);
-    }
-}
-
-const calculate = (operation) => {
-    if (canEnterOperation) {
-        const result = document.getElementById('result-value');
-        const resultString = document.getElementById('result-string');
-        if (operand1) {
-            const operand2 = Number(result.innerText);
-            result.innerText = getResult(operand1, operand2, operator);
-            operand1 = Number(result.innerText);
-        } else {
-            operand1 = Number(result.innerText);
-            result.innerText = operand1;
-        }
-        resultString.innerText = resultString.innerText + getSymbol(operation);
-        operator = operation;
-        canEnterOperation = false;
-        canEnterNumber = true;
-        restart = true;
-    }
-}
-
-const addButtonListeners = () => { 
-    const buttons = document.getElementsByClassName('number');
-    for (const button of buttons) {
-        button.addEventListener(clickEvent, () => {
-            if (canEnterNumber) {
-                const result = document.getElementById('result-value');
-                const resultString = document.getElementById('result-string');
-                let currentText = result.innerText
-                if (Number(result.innerText) === 0) {
-                    currentText = ''
-                }
-                const finalText = `${currentText}${button.innerText}`;
-                if (restart) {
-                    result.innerText = button.innerText;
-                } else {
-                    result.innerText = finalText;
-                }
-                if (resultString.innerText === '0') {
-                    resultString.innerText = ''
-                }
-                resultString.innerText = resultString.innerText + button.innerText;
-                canEnterOperation = true;
-                restart = false;
-            }
-        });
-    }
+	restart = false;
+	canEnterNumber = true;
+	canEnterOperation = false;
+	operand1 = null;
+	operator = null;
 };
 
+const getResult = (operand1, operand2, operator) => {
+	switch (operator) {
+		case 'multiply':
+			return operand1 * operand2;
+		case 'add':
+			return operand1 + operand2;
+		case 'subtract':
+			return operand1 - operand2;
+		case 'divide':
+			return Math.floor(operand1 / operand2);
+	}
+};
+
+const getSymbol = (operator) => {
+	switch (operator) {
+		case 'multiply':
+			return String.fromCharCode(215);
+		case 'add':
+			return '+';
+		case 'subtract':
+			return '-';
+		case 'divide':
+			return String.fromCharCode(247);
+	}
+};
+
+const calculate = (operation) => {
+	if (canEnterOperation) {
+		const result = document.getElementById('result-value');
+		const resultString = document.getElementById('result-string');
+		if (operand1) {
+			const operand2 = Number(result.innerText);
+			result.innerText = getResult(operand1, operand2, operator);
+			operand1 = Number(result.innerText);
+		} else {
+			operand1 = Number(result.innerText);
+			result.innerText = operand1;
+		}
+		resultString.innerText = resultString.innerText + getSymbol(operation);
+		operator = operation;
+		canEnterOperation = false;
+		canEnterNumber = true;
+		restart = true;
+	}
+};
+
+const addButtonListeners = () => {
+	const buttons = document.getElementsByClassName('number');
+	for (const button of buttons) {
+		button.addEventListener(clickEvent, () => {
+			if (canEnterNumber) {
+				const result = document.getElementById('result-value');
+				const resultString = document.getElementById('result-string');
+				let currentText = result.innerText;
+				if (Number(result.innerText) === 0) {
+					currentText = '';
+				}
+				const finalText = `${currentText}${button.innerText}`;
+				if (restart) {
+					result.innerText = button.innerText;
+				} else {
+					result.innerText = finalText;
+				}
+				if (resultString.innerText === '0') {
+					resultString.innerText = '';
+				}
+				resultString.innerText = resultString.innerText + button.innerText;
+				canEnterOperation = true;
+				restart = false;
+			}
+		});
+	}
+};
 
 const addMultiplicationListener = () => {
-    const button = document.querySelector('.multiply');
-    button.addEventListener(clickEvent, () => {
-        calculate('multiply');
-    });
-}
+	const button = document.querySelector('.multiply');
+	button.addEventListener(clickEvent, () => {
+		calculate('multiply');
+	});
+};
 
 const addAdditonListener = () => {
-    const button = document.querySelector('.add');
-    button.addEventListener(clickEvent, () => {
-        calculate('add');
-    });
-}
+	const button = document.querySelector('.add');
+	button.addEventListener(clickEvent, () => {
+		calculate('add');
+	});
+};
 
 const addSubtractionListener = () => {
-    const button = document.querySelector('.subtract');
-    button.addEventListener(clickEvent, () => {
-        calculate('subtract')
-    });
-}
+	const button = document.querySelector('.subtract');
+	button.addEventListener(clickEvent, () => {
+		calculate('subtract');
+	});
+};
 
 const addDivisionListener = () => {
-    const button = document.querySelector('.divide');
-    button.addEventListener(clickEvent, () => {
-        calculate('divide')
-    });
-}
+	const button = document.querySelector('.divide');
+	button.addEventListener(clickEvent, () => {
+		calculate('divide');
+	});
+};
 
 const addClearAllListener = () => {
-    const button = document.querySelector('.clear-all');
-    button.addEventListener(clickEvent, () => {
-        const result = document.getElementById('result-value');
-        const resultString = document.getElementById('result-string');
-        result.innerText = '0';
-        resultString.innerText = '0';
-        initializeParams();
-    });
-}
+	const button = document.querySelector('.clear-all');
+	button.addEventListener(clickEvent, () => {
+		const result = document.getElementById('result-value');
+		const resultString = document.getElementById('result-string');
+		result.innerText = '0';
+		resultString.innerText = '0';
+		initializeParams();
+	});
+};
 
 const addBackListener = () => {
-    const button = document.querySelector('.back');
-    button.addEventListener(clickEvent, () => {
-        const result = document.getElementById('result-value');
-        const resultString = document.getElementById('result-string');
-        if (result.innerText.length <= 1) {
-            result.innerText = '0'
-            resultString.innerText = '0'
-            canEnterOperation = false;
-        } else {
-            result.innerText = result.innerText.substr(0, result.innerText.length-1);
-            resultString.innerText = result.innerText;
-            operator = null;
-            operand1 = null;
-            canEnterOperation = true;
-            canEnterNumber = true;
-            restart = false;
-        }
-    });
-}
+	const button = document.querySelector('.back');
+	button.addEventListener(clickEvent, () => {
+		const result = document.getElementById('result-value');
+		const resultString = document.getElementById('result-string');
+		if (result.innerText.length <= 1) {
+			result.innerText = '0';
+			resultString.innerText = '0';
+			canEnterOperation = false;
+		} else {
+			result.innerText = result.innerText.substr(0, result.innerText.length - 1);
+			resultString.innerText = result.innerText;
+			operator = null;
+			operand1 = null;
+			canEnterOperation = true;
+			canEnterNumber = true;
+			restart = false;
+		}
+	});
+};
 
 const addEqualListener = () => {
-    const button = document.querySelector('.equals');
-    button.addEventListener(clickEvent, () => {
-        if (canEnterOperation && operator) {
-            const result = document.getElementById('result-value');
-            const resultString = document.getElementById('result-string');
-            const operand2 = Number(result.innerText);
-            result.innerText = getResult(operand1, operand2, operator);
-            resultString.innerText = result.innerText;
-            operand1 = null;
-            operator = null;
-            canEnterOperation = true;
-            canEnterNumber = false;
-            restart = true;
-        }
-    });
-}
+	const button = document.querySelector('.equals');
+	button.addEventListener(clickEvent, () => {
+		if (canEnterOperation && operator) {
+			const result = document.getElementById('result-value');
+			const resultString = document.getElementById('result-string');
+			const operand2 = Number(result.innerText);
+			result.innerText = getResult(operand1, operand2, operator);
+			resultString.innerText = result.innerText;
+			operand1 = null;
+			operator = null;
+			canEnterOperation = true;
+			canEnterNumber = false;
+			restart = true;
+		}
+	});
+};
 
 initializeParams();
 addButtonListeners();
@@ -179,4 +176,3 @@ addAdditonListener();
 addSubtractionListener();
 addDivisionListener();
 addEqualListener();
-
