0123611965dd7e5b8bd8773533b0ce22b4ca791f
Prince <--replace-all>
2020-04-13 07:34:59 +0530

    fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import { app, BrowserWindow } from 'electron';
+const { app, BrowserWindow } = require ('electron');
 
 
-const createWindow = () => {
+const createWindow =  () => {
   // Create the browser window.
   let win = new BrowserWindow({
     width: 400,
